0a3099673a9a0a275a6b62a60aba8d33bff8fc23
Mounir Bkr <93410565+MounirBkr1@users.noreply.github.com>
2022-01-02 21:03:10 +0100

    Update EmployeeJpa.java

--- 7employee-jsp-servlet-jpa/src/main/java/ma/course/dao/EmployeeJpa.java
+++ 7employee-jsp-servlet-jpa/src/main/java/ma/course/dao/EmployeeJpa.java
@@ -16,6 +16,8 @@ public class EmployeeJpa {
 	}
 	
     public List<Employee> getAllEmployees() {
+	    	//createQuery=> creer requete avec hql=manipuler les classes et les realtion entre les classe
+		//select e from Employee e => premier e comme si vs ecrivez e*=tourt les employees, 2eme e => alias
 		List<Employee> employees =em.createQuery("select e from Employee e", Employee.class)
 			.getResultList();
 		return employees;
@@ -23,14 +25,19 @@ public class EmployeeJpa {
 	
 	public void createEmployee(int id, String firstName, String lastName, String department) {
 		Employee employee=new Employee(id, firstName, lastName, department);
+		//creer une transaction, begin=> demarrer une transaction
 		em.getTransaction().begin();
 		
 		try {
+			//persist => creer un objet/sauvegarder son etat dans db
+			//va etre traduit par "insert into db object employee"
 			em.persist(employee);
+			//si tt marche bien on met commit
 			em.getTransaction().commit();		
 		}
 		catch(Exception e) {
 			e.printStackTrace();
+			//si qlq chose ne marche^pas on met rollback
 			em.getTransaction().rollback();	
 		}
 	}
